d6c7ad7d20ffa21bfbc18ec64f103841016f0234
c4llmeco4ch <cbb692@gmail.com>
2020-06-15 16:31:57 -0700

    Added code to draw hanged man

--- __main__.py
+++ __main__.py
@@ -37,20 +37,74 @@ def draw_blanks(ans: str) -> None:
         coach.up()
         coach.fd(width/(len(ans) * 2))
         coach.down()
+    draw_hanged_man([])
+
+
+def draw_hanged_man(inc: List[str]) -> None:
+    """
+    Draw the correct body part of the hanged man
+    given the number of incorrect guesses made
+    """
+    if (num := len(inc)) == 0:
+        coach.up()
+        coach.goto(-100, 0)
+        coach.down()
+        coach.seth(90)
+        coach.fd(50)
+        coach.rt(90)
+        coach.fd(200)
+        coach.right(90)
+        coach.fd(50)
+        coach.up()
+        coach.goto(-50, 50)
+        coach.down()
+        coach.seth(90)
+        coach.fd(150)
+        coach.rt(90)
+        coach.fd(100)
+        coach.rt(90)
+        coach.fd(25)
+    elif num == 1:
+        coach.seth(0)
+        coach.circle(-10, 540)
+    elif num == 2:
+        coach.left(90)
+        coach.fd(25)
+        coach.bk(20)
+    elif num == 3:
+        coach.right(15)
+        coach.fd(10)
+        coach.bk(10)
+    elif num == 4:
+        coach.left(30)
+        coach.fd(10)
+        coach.bk(10)
+        coach.right(15)
+        coach.fd(20)
+    elif num == 5:
+        coach.right(25)
+        coach.fd(7)
+        coach.bk(7)
+    else:
+        coach.left(50)
+        coach.fd(7)
+        coach.bk(7)
 
 
 def take_turn(incorrect, correct, answer):
     letter = screen.textinput('Guess', 'Guess a lettter')
     if letter is None or len(letter) != 1 or letter not in ascii_letters or (
             letter in incorrect or letter in set(correct)):
-        pass
+        print('Invalid Guess')
     elif letter in answer:
         for i, let in enumerate(answer):
             if let == letter:
                 correct[i] = letter
         # if it is, we want change it graphically
     else:
+        print('Incorrect')
         incorrect.append(letter)
+        draw_hanged_man(incorrect)
         # draw a part of the hangman
     return (incorrect, correct)
 
@@ -58,4 +112,5 @@ def take_turn(incorrect, correct, answer):
 answer, correct, wrong_guesses = setup()
 draw_blanks(answer)
 print(answer)
-wrong_guesses, correct = take_turn(wrong_guesses, correct, answer)
+while '_' in correct and len(wrong_guesses) < 6:
+    wrong_guesses, correct = take_turn(wrong_guesses, correct, answer)
